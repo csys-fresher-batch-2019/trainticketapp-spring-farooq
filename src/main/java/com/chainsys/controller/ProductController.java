6e64a4200991bea6df45fdeb564326e19d0ae80b
Mohamedfarooq S <farooqmohamed21@gmail.com>
2020-02-28 15:03:30 +0530

    update bookings api

--- src/main/java/com/chainsys/controller/ProductController.java
+++ src/main/java/com/chainsys/controller/ProductController.java
@@ -3,15 +3,18 @@ package com.chainsys.controller;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chainsys.booking.Bookingimplements;
 import com.chainsys.exception.DbException;
 import com.chainsys.viewtrains.ListTrain;
 import com.chainsys.viewtrains.ViewTrainsimplementation;
-
+@CrossOrigin( origins="*")
 @RestController
 @RequestMapping("api")
 public class ProductController {
@@ -25,4 +28,15 @@ public class ProductController {
 		LocalDate date = LocalDate.parse(traveldate);
 		return obj.getTrainDetails(boardingStation, destinationStation, date);
 	}
+	@PostMapping("/bookings")
+
+	public int bookings(@RequestParam("userid") int userId,
+			@RequestParam("trainnumber") int trainnumber, @RequestParam("source") String boarding,@RequestParam("destination") String destination,@RequestParam("noofseats") int noOfSeats,@RequestParam("date") String traveldate)
+			throws Exception {
+
+		Bookingimplements obj = new Bookingimplements();
+		LocalDate date = LocalDate.parse(traveldate);
+		return obj.bookSeats(trainnumber, userId, boarding, destination, noOfSeats, date);
+	}
+
 }

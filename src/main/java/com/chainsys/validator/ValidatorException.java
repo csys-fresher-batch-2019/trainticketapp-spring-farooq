60e905d7eeb8893cf642e3937d6a909beecaa3f2
Mohamedfarooq S <farooqmohamed21@gmail.com>
2020-03-03 09:32:49 +0530

    updating

--- src/main/java/com/chainsys/validator/ValidatorException.java
+++ src/main/java/com/chainsys/validator/ValidatorException.java
@@ -1,13 +1,15 @@
 package com.chainsys.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class ValidatorException extends Exception {
 
 	
 	private static final long serialVersionUID = 1L;
-
+@Autowired
 	public ValidatorException(String invaliduserId) {
 		super(invaliduserId);
 	}

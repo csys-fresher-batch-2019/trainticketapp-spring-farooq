bcd2f15b42b13d5c322700dc0b948e0140199c49
Mohamedfarooq S <farooqmohamed21@gmail.com>
2020-03-03 18:06:41 +0530

    updating

--- src/main/java/com/chainsys/dao/RegistrationDAO.java
+++ src/main/java/com/chainsys/dao/RegistrationDAO.java
@@ -16,16 +16,16 @@ public interface RegistrationDAO {
 	
 	public int registrationInsert(String username,String password,String emailid,long phonenumber,String gender,LocalDate dob,String cityname) throws DbException;
 	
-	@SqlUpdate("update registration set pass= :password where email_id= :email")
+	
 	public void changePassword(@Bind("email")String emailid,@Bind("password")String pass) throws DbException;
 	
-	@SqlQuery("select * from registration")
+	
 	public ArrayList<Register>getAllUserDetails()throws DbException;
 	
-	@SqlUpdate("delete from booking where booking.booked_date=(select max(booked_date) from booking)")
+	
 	public void deleteUser()throws DbException;
 		
-	@SqlUpdate("update registration set blocklist = :blocklist where user_id= :id")
+	
 	public int blockUser(@Bind("id") int userid,@Bind("blocklist") int status) throws DbException;
 	
 	

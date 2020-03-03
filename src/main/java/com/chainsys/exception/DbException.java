25c725fdcb36556eb221d57b32bb714dd21d9366
Mohamedfarooq S <farooqmohamed21@gmail.com>
2020-03-03 09:55:33 +0530

    updating

--- src/main/java/com/chainsys/exception/DbException.java
+++ src/main/java/com/chainsys/exception/DbException.java
@@ -1,10 +1,8 @@
 package com.chainsys.exception;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class DbException extends Exception {
 
-	@Autowired
 	private static final long serialVersionUID = 1L;
 
 	public DbException(String msg) {
